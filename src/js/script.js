117e2cd19a4ca4acb99031e25f4111cd90d29098
Pascal Dietrich <pascal.1.dietrich@hotmail.com>
2023-05-22 11:17:29 +0200

    m

--- src/js/script.js
+++ src/js/script.js
@@ -101,3 +101,30 @@ function randomReOrder() {
 function up() {
 	$('.navbar')[0].scrollIntoView();
 }
+
+
+const scrollOffset = 100;
+const scrollElement = document.querySelector(".scroll");
+const elementInView = (el, offset = 0) => {
+  const elementTop = el.getBoundingClientRect().top;
+  return (
+    elementTop <= 
+    ((window.innerHeight || document.documentElement.clientHeight) - offset)
+  );
+};
+const displayScrollElement = () => {
+  scrollElement.classList.add('scrolled');
+}
+const hideScrollElement = () => {
+  scrollElement.classList.remove('scrolled');
+}
+const handleScrollAnimation = () => {
+  if (elementInView(scrollElement, scrollOffset)) {
+      displayScrollElement();
+  } else {
+    hideScrollElement();
+  }
+}
+window.addEventListener('scroll', () => {
+  handleScrollAnimation();
+})
